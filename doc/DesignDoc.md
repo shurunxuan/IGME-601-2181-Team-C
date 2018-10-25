80e8c79aa4731f03c3ebc0bbdde04eac559f7e61
Victor Shu (RIT Student) <vs1571@ad.rit.edu>
2018-10-25 15:26:25 -0400

    Added comments and acronyms

--- doc/DesignDoc.md
+++ doc/DesignDoc.md
@@ -28,12 +28,11 @@ For more details, see [the full market & user analysis document](MarketUserAnaly
 ![SellSheet](Assets/SellSheet.png)
 
 ### Glossary and Acronyms
-> Provide a table of terms and acronyms.
 
 | Term | Definition |
 |------|------------|
-| term/acronym | definition |
-
+| Horizontal Movement | The drone moves in regards to the X-Z plane. |
+| Vertical Movement | The drone moves along the Y axis. |
 
 ## Requirements
 
@@ -41,7 +40,9 @@ For more details, see [the full market & user analysis document](MarketUserAnaly
 
 This section describes the main features of the game.
 
-> In this section you do not need to be exhaustive and list every story.  Focus on top-level features and maybe Epics and *critical* Stories.
+ * Movement
+   * Horizontal Movement
+   * Vertical Movement
 
 ### Non-functional Requirements
 > Key NFRs and technical constraints
@@ -68,17 +69,9 @@ Our architecture will need to connect to and build on top of Unity's existing ar
 
 For more details, see [the full architecture document](architecture.md).
 
-### Component 1 ...
-> Provide a summary of each component with extra models as needed
-
-
-### Component 1 ...
-> Provide a summary of each component with extra models as needed
-
 ## Detailed Design
 
 > You'll add to this section as needed as the project progresses
-
 
 ## Issues and Risks
 
--- src/Infiltrator_D/Assets/Scripts/Drone/CameraController.cs
+++ src/Infiltrator_D/Assets/Scripts/Drone/CameraController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // The object that the camera is following
     public GameObject FollowingObject;
+    // The direction that the camera is currently looking at
     public Vector3 LookAtDirection;
 
+    // Speed Factors
     public float HorizontalViewSpeed;
     public float VerticalViewSpeed;
 
+    // Calculated position that the camera should be at
     private Vector3 targetPosition;
+    // Variable for not changing the magnitude of LookAtDirection
     private float distance;
+
     // Use this for initialization
     void Start()
     {
+        // Initialize LookAtDirection
         LookAtDirection = FollowingObject.transform.position + 0.1f * Vector3.up - transform.position;
         distance = LookAtDirection.magnitude;
     }
@@ -22,12 +29,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Get User Input
         float horizontalCamera = Input.GetAxis("HorizontalCamera");
         float verticalCamera = Input.GetAxis("VerticalCamera");
 
+        // Rotate the LookAtDirection regarding user input
         LookAtDirection = Quaternion.AngleAxis(horizontalCamera * HorizontalViewSpeed, Vector3.up) * LookAtDirection;
         LookAtDirection = Quaternion.AngleAxis(-verticalCamera * VerticalViewSpeed, Vector3.Cross(Vector3.up, LookAtDirection)) * LookAtDirection;
-        Debug.Log(Mathf.Acos(LookAtDirection.y / LookAtDirection.magnitude) * Mathf.Rad2Deg);
+
+        // Restrict the pitch angle of LookAtDirection
         if (Mathf.Acos(LookAtDirection.y / LookAtDirection.magnitude) < Mathf.Deg2Rad * 10)
         {
             LookAtDirection = new Vector3(LookAtDirection.x, 0, LookAtDirection.z);
@@ -40,8 +50,10 @@ public class CameraController : MonoBehaviour
             LookAtDirection.y = LookAtDirection.magnitude / Mathf.Tan(Mathf.Deg2Rad * 170);
             LookAtDirection = LookAtDirection.normalized * distance;
         }
+        // Set the forward vector of drone.
         FollowingObject.GetComponent<DroneMovement>().Forward = Vector3.Cross(Vector3.Cross(Vector3.up, LookAtDirection), Vector3.up);
 
+        // Position and rotation transition
         targetPosition = FollowingObject.transform.position - LookAtDirection + 0.1f * Vector3.up;
         transform.position = Vector3.Lerp(transform.position, targetPosition, 20f * Time.fixedDeltaTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(LookAtDirection, Vector3.up), 20f * Time.fixedDeltaTime);
