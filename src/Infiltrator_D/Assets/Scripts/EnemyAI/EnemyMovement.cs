ee1c090939014d58f50d0362d64aa979b7aeb083
Shantanu Jamble <sj6045@rit.edu>
2018-10-29 01:57:24 -0400

    Enemy Patrol Movement and Enemy Sight ability code changes done. To test just the Enemey AI related scene in the scens directory. User stories: https://trello.com/c/uGBT00md https://trello.com/c/DGuRtloz

---
+++ src/Infiltrator_D/Assets/Scripts/EnemyAI/EnemyMovement.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    //enemy state enums defines all the possible states enemy can switch to.
+    public enum  enemystate{
+        PATROL =1,
+        INVESTIGATE=2
+    }
+
+    //basic movement parameters
+    public Camera cam;
+    public NavMeshAgent agent;
+    public GameObject enemy_object;
+    public GameObject [] patrol_points = new GameObject[6];
+    public int startpoint = -1;
+    public int speed;
+    public float waititme = 3f;
+    private float timer = 0f;
+    public enemystate enemy_state = enemystate.PATROL;
+
+
+    //investigation parameters
+    private EnemySight sight = null;
+    private Transform target;
+    private bool targetUpdated = false;
+    public LayerMask playermask;
+    public LayerMask obstaclemask;
+
+    void Start()
+    {
+        sight = new EnemySight(transform,playermask,obstaclemask);
+        agent.speed = speed;
+       
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+        sight.Look();
+
+        //Enemy behaviour can be modified depending upon the current state of enemy.
+        switch (enemy_state)
+        {
+            case enemystate.PATROL:
+                Patrol();
+                break;
+            case enemystate.INVESTIGATE:
+                if (targetUpdated)
+                {
+                    Investigate();
+                    targetUpdated = false;
+                }
+                break;
+        }
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 2f, Color.green);
+
+        
+    }
+
+     void FixedUpdate()
+    {
+
+        //check if player is in sight and update the beaviour
+        
+        if(sight.isPlayerVisible(out target))
+        {
+            Debug.Log("Updating state to Investigate");
+            enemy_state = enemystate.INVESTIGATE;
+            targetUpdated = true;
+        }
+    }
+
+    private void Patrol()
+    {
+        if (timer == 0)
+        {
+            startpoint = (startpoint < patrol_points.Length - 1) ? startpoint + 1 : 0;
+            agent.SetDestination(patrol_points[startpoint].transform.position);
+     
+            timer = waititme;
+        }
+        else
+        {
+            
+            if (Vector3.Distance(transform.position, patrol_points[startpoint].transform.position) < 2f)
+            {
+                timer--;
+            }
+        }
+
+    }
+
+    private void Investigate()
+    {
+        agent.SetDestination(target.position);
+    }
+    
+
+
+
+}
---
+++ src/Infiltrator_D/Assets/Scripts/EnemyAI/EnemySight.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySight {
+
+    //Sight/looking ability related paramerters
+    public float view_radius = 5f;
+    public Transform transform;
+    public float view_angle = 90;
+    private LayerMask playermask;
+    private LayerMask obstaclemask;
+    
+    public EnemySight(Transform transform,LayerMask playermask,LayerMask obstaclemask)
+    {
+        this.transform = transform;
+        this.playermask = playermask;
+        this.obstaclemask = obstaclemask;
+    }
+
+    public Vector3 GetDirFromAngle(float angleindegrees)
+    {
+
+        float angle_a = angleindegrees + transform.eulerAngles.y;
+        Vector3 dir_a = new Vector3(Mathf.Sin(angle_a * Mathf.Deg2Rad), 0, Mathf.Cos(angle_a * Mathf.Deg2Rad));
+        return dir_a;
+    }
+
+
+    public void Look()
+    {
+        //Get direction from angle
+
+        Vector3 dir_a = GetDirFromAngle(-view_angle / 2);
+        Vector3 dir_b = GetDirFromAngle(view_angle / 2);
+        Debug.DrawLine(transform.position, transform.position + dir_a * view_radius);
+        Debug.DrawLine(transform.position, transform.position + dir_b * view_radius);
+
+    }
+
+    public bool isPlayerVisible(out Transform player)
+    {
+
+        //checks if target is in the view radius
+        Collider[] playerInViewRadius = Physics.OverlapSphere(transform.position, view_radius, playermask);
+
+        foreach (Collider c in playerInViewRadius)
+        {
+            
+            Transform target = c.transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            Debug.Log(Vector3.Angle(transform.position, dirToTarget));
+            //Check if target is visible
+            if (Vector3.Angle(transform.position, target.position) < view_angle/2 )
+            {
+              
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                Debug.DrawLine(transform.position, target.position, Color.red);
+                if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, obstaclemask))
+                {
+                    Debug.Log("Found you!!");
+                    player = target;
+
+                    Debug.DrawLine(transform.position, target.position,Color.red);
+                    return true;
+                }
+            }
+
+        }
+        player = null;
+        return false;
+    }
+    
+       
+}
