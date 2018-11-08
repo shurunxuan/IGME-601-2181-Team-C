15a81ff9e6cf2555b3b669bf4aaea3a2d704c607
Mark DelFavero <markadelf@gmail.com>
2018-11-08 14:26:53 -0500

    Work on Decoy and Projection

---
+++ src/Infiltrator_D/Assets/Scripts/Drone/Tools/DecoyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoyScript : MonoBehaviour {
+
+    // The time period for which the decoy will remain active
+    public float TimeActive;
+
+    // The timer currently in use for cleaning up the decoy
+    // timer is -1 if inactive
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = -1;
+        enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            // Destroy self after timer runs out
+            Destroy(gameObject);
+        }
+	}
+
+    // Becomes active on hit
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (timer == -1)
+        {
+            timer = TimeActive;
+        }
+        enabled = true;
+    }
+}
--- src/Infiltrator_D/Assets/Scripts/Drone/Tools/DecoyTool.cs
+++ src/Infiltrator_D/Assets/Scripts/Drone/Tools/DecoyTool.cs
@@ -9,56 +9,27 @@ public class DecoyTool : ToolComponent {
 
     // The device launched by the tool
     public GameObject DecoyDevice;
-
+    
     // The speed at which the decoy leaves the muzzle
     public float MuzzleSpeed;
 
-    // Length in seconds of the predicted trajectory
-    public float PredictionLength;
-
-    // Time of one step for the prediction of trajectory
-    public float PredictionStep;
-
-    // Line for drawing projectory
-    private LineRenderer predictedTrajectory;
-
-    // Bounciness of the decoy
-    private float bounciness = 0;
+    // The TrajectoryIndicatior for this tools trajectory
+    private TrajectoryIndicator indicator;
 
     // Use this for initialization
-    void Start () {
-        predictedTrajectory = GetComponent<LineRenderer>();
-        bounciness = DecoyDevice.GetComponent<Collider>().material.bounciness;
-	}
+    void Start()
+    {
+        indicator = Muzzle.GetComponent<TrajectoryIndicator>();
+        if (indicator != null)
+        {
+            indicator.MuzzleSpeed = MuzzleSpeed;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        // Predict the trajectory of the shot
-        if (predictedTrajectory != null)
-        {
-            List<Vector3> positions = new List<Vector3>();
-            Vector3 grav = Physics.gravity;
-            Vector3 vel = Muzzle.forward * MuzzleSpeed;
-            Vector3 pos = Muzzle.position;
-            positions.Add(pos);
-            for (float i = 0; i < PredictionLength; i += PredictionStep)
-            {
-                vel += grav * PredictionStep;
-                RaycastHit hit;
-                float dist = vel.magnitude * PredictionStep;
-                if (Physics.Raycast(pos, vel, out hit, dist))
-                {
-                    positions.Add(hit.point);
-                    break;
-                }
-                pos += vel * PredictionStep;
-
-                positions.Add(pos);
-            }
-            predictedTrajectory.positionCount = positions.Count;
-            predictedTrajectory.SetPositions(positions.ToArray());
-        }
+        
 	}
 
     // Launch a decoy pellet
