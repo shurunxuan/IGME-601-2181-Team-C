7f3312f8642a63dbbde257c30aadf4583ca85b4f
Victor Shu <shurunxuan@hotmail.com>
2018-11-12 18:54:52 -0500

    Fixed the camera bug

---
+++ src/Infiltrator_D/Assets/Scripts/Drone/FirstPersonFollowing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonFollowing : MonoBehaviour {
+
+    public Transform DroneTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        transform.position = DroneTransform.position;
+    }
+}
