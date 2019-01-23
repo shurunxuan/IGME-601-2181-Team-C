4b32cdbac37100b489775b6a47d00e6156b3eaaa
Mark DelFavero <markadelf@gmail.com>
2019-01-23 15:26:15 -0500

    Modified to use scriptable objects

--- src/Infiltrator_D/Assets/Scripts/UI/MissionInfo.cs
+++ src/Infiltrator_D/Assets/Scripts/UI/MissionInfo.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 // This class is purely for storing data created in the editor
-public class MissionInfo : MonoBehaviour {
-
+[CreateAssetMenu(fileName = "MissionInfo", menuName = "MissionInfo", order = 1)]
+public class MissionInfo : ScriptableObject
+{
     public string Scene;
 
     public string Title;
